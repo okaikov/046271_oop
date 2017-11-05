021338d11d2f9e462f764ba3787a955149065287
okaikov <oren.kaikov@gmail.com>
2017-11-05 23:34:40 +0200

    initial implementation of section 2

---
+++ src/HW1/Flesch.java
@@ -0,0 +1,79 @@
+package HW1;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Flesch {
+    public static void main(String[] args) throws IOException {
+        String sentenceDelimiter = "[\\.,;\\?\\!]";
+        String wordDelimiter = "\\W";
+        List<String> sentenceList = new ArrayList<String>();
+        List<String> wordList = new ArrayList<String>();
+
+        checkArgs(args);
+        String filePath = args[0];
+        // read file
+        String entireFileText = new Scanner(new File(filePath)).useDelimiter("\\A").next();
+        String[] sentences = entireFileText.split(sentenceDelimiter);
+        for (String sentence : sentences) {
+            sentence = sentence.trim().replace("\n", "");
+            if (!sentence.isEmpty()) {
+                sentenceList.add(sentence);
+            }
+        }
+
+        for (String sentence : sentenceList){
+            String[] words= sentence.split(wordDelimiter);
+            for (String word : words ) {
+                if (!word.isEmpty()) {
+                    wordList.add(word);
+                }
+            }
+        }
+
+    }
+
+    private static int getNumvowels(String word){
+        String vowelsString = "aAeEiIoOuUyY";
+        boolean prevCharIsVowel = false;
+        boolean startOfWord = true;
+        int numVowels = 0;
+
+        for (char c : word.toCharArray()){
+            if (vowelsString.indexOf(c)>=0){
+                if (!startOfWord && prevCharIsVowel){
+                    numVowels--;
+                    continue;
+                }
+                numVowels++;
+                prevCharIsVowel = true;
+            }
+            if (startOfWord) startOfWord = false;
+
+        }
+        if (numVowels == 0) numVowels = 1;
+        return numVowels;
+    }
+
+
+    private static void checkArgs(String[] args){
+        if (args.length==0){
+            System.out.println("Error: no filename was given");
+            System.exit(0);
+        }else if (args.length>1){
+            System.out.println("Error: more then 1 arguments were given");
+            System.exit(0);
+        }
+        String filePath = args[0];
+        File f = new File(filePath);
+        if(!f.exists() || f.isDirectory()) {
+            System.out.println("Error: file does not exist or is a directory");
+            System.exit(0);
+        }
+    }
+
+
+
+}
